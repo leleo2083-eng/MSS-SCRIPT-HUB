ebb9d3e46a7ffbaa99fb9719241191174596b96d
leleo2083-eng <leleo2083@gmail.com>
2026-07-03 01:07:21 +0200

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,20 @@
+local scripts = {
+    [123456789] = "https://raw.githubusercontent.com/USERNAME/REPO/main/script1.lua",
+    [987654321] = "https://raw.githubusercontent.com/USERNAME/REPO/main/script2.lua",
+    [111111111] = "https://raw.githubusercontent.com/USERNAME/REPO/main/script3.lua",
+}
+
+local placeId = game.PlaceId
+local scriptUrl = scripts[placeId]
+
+if scriptUrl then
+    local success, err = pcall(function()
+        loadstring(game:HttpGet(scriptUrl, true))()
+    end)
+
+    if not success then
+        warn("Failed to load script:", err)
+    end
+else
+    warn("No script assigned for PlaceId:", placeId)
+end
